65353239041fae0fada69977d5094cfd64e150d0
E.Z. <86618956+PiethonProgram@users.noreply.github.com>
2024-09-28 23:36:06 -0400

    Update utils.py

--- antropy/tests/utils.py
+++ antropy/tests/utils.py
@@ -5,22 +5,15 @@ RANDOM_TS = np.random.rand(3000)
 NORMAL_TS = np.random.normal(size=3000)
 RANDOM_TS_LONG = np.random.rand(6000)
 PURE_SINE = np.sin(2 * np.pi * 1 * np.arange(3000) / 100)
+PURE_COSINE = np.cos(2 * np.pi * 1 * np.arange(3000) / 100)
 ARANGE = np.arange(3000)
 
 # Data types for which to test input array compatibility
 TEST_DTYPES = [
     # floats
-    np.float16,
-    np.float32,
-    np.float64,
-    np.int8,
+    np.float16, np.float32, np.float64, np.int8,
     # ints
-    np.int16,
-    np.int32,
-    np.int64,
+    np.int16, np.int32, np.int64,
     # unsigned ints
-    np.uint8,
-    np.uint16,
-    np.uint32,
-    np.uint64,
+    np.uint8, np.uint16, np.uint32, np.uint64,
 ]

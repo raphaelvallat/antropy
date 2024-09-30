51faba332a18b227d8040f00f7591097c240e312
ethan <ethan.lee.zhu@gmail.com>
2024-09-30 11:36:47 -0400

    katz_fd(x_k)==1.503

--- antropy/tests/test_fractal.py
+++ antropy/tests/test_fractal.py
@@ -7,7 +7,7 @@ from numpy import apply_along_axis as aal
 from antropy import petrosian_fd, katz_fd, higuchi_fd, detrended_fluctuation
 
 
-from utils import RANDOM_TS, NORMAL_TS, PURE_SINE, ARANGE, TEST_DTYPES
+from utils import RANDOM_TS, NORMAL_TS, PURE_SINE, PURE_COSINE, ARANGE, TEST_DTYPES
 
 PPG_SIGNAL = np.array(
     [
@@ -54,10 +54,11 @@ class TestEntropy(unittest.TestCase):
 
     def test_katz_fd(self):
         x_k = [0.0, 0.0, 2.0, -2.0, 0.0, -1.0, -1.0, 0.0]
-        self.assertEqual(np.round(katz_fd(x_k), 3), 5.783)
+        self.assertEqual(np.round(katz_fd(x_k), 3), 1.503)
         # 2D data
-        assert_equal(aal(katz_fd, axis=1, arr=data), katz_fd(data))
-        assert_equal(aal(katz_fd, axis=0, arr=data), katz_fd(data, axis=0))
+        data_kfd = np.vstack((RANDOM_TS, NORMAL_TS, PURE_SINE, PURE_COSINE, ARANGE))
+        assert_equal(aal(katz_fd, axis=1, arr=data_kfd), katz_fd(data_kfd))
+        assert_equal(aal(katz_fd, axis=0, arr=data_kfd), katz_fd(data_kfd, axis=0))
 
     def test_higuchi_fd(self):
         """Test for function `higuchi_fd`.

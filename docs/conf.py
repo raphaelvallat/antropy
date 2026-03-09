0ef23fac6e20234c9766697d36614a235645d190
Raphael Vallat <raphaelvallat9@gmail.com>
2026-03-09 16:25:27 +0100

    MAINT: Fix imports and clean up project configuration

--- docs/conf.py
+++ docs/conf.py
@@ -5,15 +5,7 @@ For the full list of built-in configuration values, see the documentation:
 https://www.sphinx-doc.org/en/master/usage/configuration.html
 """
 
-import os
-import sys
-
 import antropy
-
-# -- Path setup --------------------------------------------------------------
-
-sys.path.insert(0, os.path.abspath(os.path.pardir))
-
 
 # -- Project information -----------------------------------------------------
 
